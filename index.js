4cc7a059ce806f0799e826ffc4357647ddd72909
Parth Bakhda <Bakhdaparth@gmail.comgit config --global user.email Bakhdaparth@gmail.comgit config --global user.email Parth>
2023-04-05 19:28:29 -0700

    Second push

--- index.js
+++ index.js
@@ -16,3 +16,22 @@ inquirer
           return 'Please enter only letters and numbers.';
         } else {
           return true;
+        }
+      },
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message:
+        'Enter a color keyword (e.g. "red") or a hexadecimal color code (e.g. "#FF0000") for the text:',
+      validate: function (value) {
+        var regex = /^#?[0-9A-Fa-f]{6}$/;
+        if (!value.match(/^[a-zA-Z]+$/) && !regex.test(value)) {
+          return 'Please enter a valid color keyword or a hexadecimal color code.';
+        } else {
+          return true;
+        }
+      },
+    },
+    {
+    
